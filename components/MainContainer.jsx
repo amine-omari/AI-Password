ab890f9078bc1cbff2fd4edca7f918048ba8d0ab
amine alaoui <omari1amineco@gmail.com>
2024-02-08 10:49:39 +0100

    feat: add style to h1 title

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const MainContainer = () => {
-  return <div className="card"></div>;
+  return (
+    <div className="card">
+      <h1>PASSWORD GENERATOR</h1>
+    </div>
+  );
 };
 
 export default MainContainer;
