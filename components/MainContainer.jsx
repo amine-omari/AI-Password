d39b5c98a97e0e5ca23ffb3ac8d8a73369900912
amine alaoui <omari1amineco@gmail.com>
2024-02-08 11:23:10 +0100

    feat: add options to select field

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -4,7 +4,24 @@ const MainContainer = () => {
   return (
     <div className="card">
       <h1>PASSWORD GENERATOR</h1>
-      <form></form>
+      <form>
+        <label>choose number of characters :</label>
+        <select name="" id="">
+          <optgroup label="number of characters">
+            <option value="">8</option>
+            <option value="">10</option>
+            <option value="">12</option>
+          </optgroup>
+        </select>
+        <label>choose level of passowrd :</label>
+        <select name="" id="">
+          <optgroup label="level of passowrd">
+            <option value="easy">easy</option>
+            <option value="strong">strong</option>
+            <option value="veryStrong">very strong</option>
+          </optgroup>
+        </select>
+      </form>
     </div>
   );
 };
