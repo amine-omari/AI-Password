60c46672f7e085b38360702d65cfc1492278c359
amine alaoui <omari1amineco@gmail.com>
2024-02-08 12:26:31 +0100

    feat: add password state to MainContainer component

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -34,25 +34,29 @@ const MainContainer = () => {
   return (
     <div className="card">
       <h1>PASSWORD GENERATOR</h1>
-      <form onSubmit={handleSubmit}>
-        <label>choose number of characters :</label>
-        <select name="numCharacters" id="numCharacters">
-          <optgroup label="number of characters">
-            <option value="8">8</option>
-            <option value="10">10</option>
-            <option value="12">12</option>
-          </optgroup>
-        </select>
-        <label>choose level of passowrd :</label>
-        <select name="level" id="level">
-          <optgroup label="level of passowrd">
-            <option value="easy">easy</option>
-            <option value="strong">strong</option>
-            <option value="veryStrong">very strong</option>
-          </optgroup>
-        </select>
-        <button type="submit">generate passowrd</button>
-      </form>
+      {password ? (
+        <h2>{password}</h2>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label>choose number of characters :</label>
+          <select name="numCharacters" id="numCharacters">
+            <optgroup label="number of characters">
+              <option value="8">8</option>
+              <option value="10">10</option>
+              <option value="12">12</option>
+            </optgroup>
+          </select>
+          <label>choose level of passowrd :</label>
+          <select name="level" id="level">
+            <optgroup label="level of passowrd">
+              <option value="easy">easy</option>
+              <option value="strong">strong</option>
+              <option value="veryStrong">very strong</option>
+            </optgroup>
+          </select>
+          <button type="submit">generate passowrd</button>
+        </form>
+      )}
     </div>
   );
 };
